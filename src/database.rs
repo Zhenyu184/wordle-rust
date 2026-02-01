492beefe0f5390d1af148bea9e1592eaa8952b39
Zhenyu184 <zyy1120126@gmail.com>
2026-02-01 16:36:27 +0800

    feat: Update game status table and modify add_game function to include game type

--- src/database.rs
+++ src/database.rs
@@ -32,9 +32,10 @@ impl Database {
         
         self.conn.execute(
             "CREATE TABLE IF NOT EXISTS status (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id      INTEGER PRIMARY KEY AUTOINCREMENT,
                 game_id INTEGER NOT NULL,
-                answer TEXT NOT NULL,
+                type    INTEGER NOT NULL,
+                answer  TEXT NOT NULL,
                 guesses TEXT,
                 FOREIGN KEY(game_id) REFERENCES games(id)
             )",
@@ -44,15 +45,15 @@ impl Database {
         Ok(())
     }
 
-    pub fn add_game(&self, answer: &str) -> Result<i64> {
+    pub fn add_game(&self, game_type: u8, answer: &str,) -> Result<i64> {
         self.conn.execute(
             "INSERT INTO games DEFAULT VALUES", []
         )?;
 
         let id = self.conn.last_insert_rowid();
         self.conn.execute(
-            "INSERT INTO status (game_id, answer) VALUES (?1, ?2)",
-            params![id, answer],
+            "INSERT INTO status (game_id, type, answer) VALUES (?1, ?2, ?3)",
+            params![id, game_type, answer],
         )?;
         
         Ok(id)
--- src/game.rs
+++ src/game.rs
@@ -3,7 +3,7 @@ use crate::database;
 
 pub fn new() {
     let answer = answer::generate_answer();
-    let id = database::Database::connect().add_game(&answer).expect("Failed to add game");
+    let id = database::Database::connect().add_game(0, &answer).expect("Failed to add game");
     println!("Answer: {}", answer);
     println!("Game started! (id: {})", id);
 }
